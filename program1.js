77f4a9b9356cf7cf9386c85221ecb12f6a773a86
Vikash Mandal <aaryavikash161gmail.com>
2024-09-25 15:44:20 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    var isValid = function(s) {
+        const stack = [];
+        const parenthesesMap = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        };
     
+        for (let char of s) {
+            if (parenthesesMap[char]) {
+                stack.push(char);
+            } else {
+                if (stack.length === 0 || parenthesesMap[stack.pop()] !== char) {
+                    return false; 
+                }
+            }
+        }
+    };
 };
 
 module.exports = { isValid };
