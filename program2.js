dc9cbcdbefb2b8c5f370e393f1344f807420d656
Vikash Mandal <aaryavikash161gmail.com>
2024-09-25 16:11:08 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,29 +1,10 @@
-var romanToInt = (s) =>{
-    const romanMap = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    };
-
-    let total = 0;
-    const length = s.length;
-
-    for (let i = 0; i < length; i++) {
-        const currentVal = romanMap[s[i]];
-        const nextVal = romanMap[s[i + 1]];
-
-        if (nextVal > currentVal) {
-            total -= currentVal;
-        } else {
-            total += currentVal;
-        }
-    }
-
-    return total;
-
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    
 };
+
+
 module.exports={romanToInt}
